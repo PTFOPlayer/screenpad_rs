ca950242c4e17cef4973e57042e6f22c7de4693a
PTFOPlayer <PTFOPlayer>
2023-01-19 08:51:15 +0100

    added up and down commad

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate lazy_static;
 
-use std::{env, process::{Command}, thread::sleep, time::Duration};
+use std::{env, process::{Command, exit}, thread::sleep, time::Duration};
 mod installer;
 
 lazy_static! {
@@ -14,7 +14,10 @@ lazy_static! {
     static ref ARG_2:String = ARGS[2].clone();
 
     // change this to true if you want to use build in installer
+    // installer installs Plippos Asus-wmi-screenpad
+    // please do not use it
     static ref INSTALLER_STATUS: bool = false;
+    // static ref INSTALLER_STATUS: bool = true;
 }
 
 fn main() {
@@ -27,20 +30,15 @@ fn arg_parser() {
             if *INSTALLER_STATUS {
                 if installer::install_asus_wmi() {println!("After reboot run:\n sudo chmod a+w '/sys/class/leds/asus::screenpad/brightness'", )} 
                 else {println!("Error installing")}
-            } else {println!("installer disabled, check github page for more info")}
+            } else {println!("installer disabled, you can enable it by commenting line 19 and uncommenting line 20")}
         }
         "--brightness" | "-b" => {
             let brightness= ARG_2.as_str();
             let b_int:i32 = match  brightness.parse() {
                 Ok(res) => res,
-                Err(_) => {println!("wrong value provided, setting value to half (50)"); 50}
+                Err(_) => {println!("wrong value provided, setting value to 100"); 100}
             };
-            if b_int > 0 && b_int <= 100 { execute(&format!("{}", b_int));}
-            else {println!("wrong value, correct values are in range of 1..100")}
-        }
-        "--overload" | "-o" => {
-            let brightness= ARG_2.as_str();
-            if brightness > "0" && brightness <= "255" {execute(brightness);} 
+            if b_int > 0 && b_int <= 255 { execute(&format!("{}", b_int));}
             else {println!("wrong value, correct values are in range of 1..255")}
         }
         "--off" | "-f" => { execute("0"); }
@@ -51,9 +49,7 @@ fn arg_parser() {
             .arg("/sys/class/backlight/intel_backlight/brightness");
             match out.output() {
                 Ok(out)=>{
-                    println!("{}", String::from_utf8(out.clone().stdout).unwrap());
                     let val_str = String::from_utf8(out.stdout[..(out.stdout.len() -1)].to_vec()).unwrap();
-                    println!("{}", val_str);
                     let val = val_str.parse::<i32>().expect("error ocured while getting main screen brightness");
                     let val = val/100;
                     execute(&format!("{}", val));
@@ -61,21 +57,34 @@ fn arg_parser() {
                 Err(err)=>{println!("error: {}",err)}
             }
         }
-        "--current" | "-c" => {
-            let mut set = Command::new("sh");
-            let out = set
-            .arg("/usr/bin/current.sh");
-            match out.output() {
-                Ok(res) => {println!("current screenpad brightness: {}", String::from_utf8(res.stdout).unwrap())}
-                Err(_) => {println!("error ocured reading brightness")}
-                
+        "--current" | "-c" => println!("current screenpad brightness: {}", current()),
+        "--up" | "-u" => {
+            match current().parse::<i32>() {
+                Ok(res) => {
+                    if res+10 < 255 { execute(&format!("{}", res+10)) } 
+                    else { println!("cannot increase more") }
+                }
+                Err(_) => println!("error occured increasing brightness")
+            }
+        }
+        "--down" | "-d" => {
+            match current().parse::<i32>() {
+                Ok(res) => {
+                    if res-10 > 0 { execute(&format!("{}", res-10)) } 
+                    else { println!("cannot decrease more") }
+                }
+                Err(_) => println!("error occured decreasing brightness")
             }
         }
         "--help" | "-h" => { println!("
---brightness | -b : changes brightness in values from 1 to 100,
---overload | -o : changes brightness in extended range from 1 to 255,
---off | -f : turns screenpad off,
---on | -n : turns screenpad on")
+--brightness    | -b    : changes brightness in values from 1 to 255,
+--sync          | -s    : synchronizes brightness of screenpad with main screen
+--current       | -c    : gives current brightness of screenpad
+--off           | -f    : turns screenpad off,
+--on            | -n    : turns screenpad on,
+--up            | -u    : increases brightness by 10
+--down          | -d    : decreases brightness by 10
+");
         }
         &_ => {println!("wrong argument, see --help");}
     }
@@ -89,5 +98,15 @@ fn execute(brightness: &str) {
     match out.output() {
         Ok(_)=>{println!("succesfully changed brightness")}
         Err(err)=>{println!("error: {}",err)}
+    }
+}
+
+fn current() -> String{
+    let mut set = Command::new("sh");
+        let out = set
+        .arg("/usr/bin/current.sh");
+        match out.output() {
+            Ok(out) => {String::from_utf8(out.stdout[..(out.stdout.len() -1)].to_vec()).unwrap()}
+            Err(_) => {println!("error ocured reading brightness"); exit(1)}   
     }
 }
