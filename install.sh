cd95e3ab758aa63b8534e1a07f9d5ba2a992f49e
WhiskyAKM <35374730+PTFOPlayer@users.noreply.github.com>
2023-11-26 15:26:59 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,3 @@
-#cargo build --release
-#sudo cp target/release/screenpad_rs /usr/bin
-#sudo cp ./src/brightness.sh /usr/bin/brightness.sh
-#sudo cp ./src/current.sh /usr/bin/current.sh
-#sudo cp ./screenpad_rs.service /etc/systemd/system/
-#sudo mkdir /var/screenpad_rs/
-#sudo echo 100 > /var/screenpad_rs/brightness
-
 mkdir build
 cd build
 tar -xvf ../screenpad_rs_binaries.tar.gz
